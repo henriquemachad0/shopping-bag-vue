2895034e9f8b27e7a4cf8b9ab54a782e2542e1a6
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-05-23 18:54:16 -0300

    feat: removing items from bag

--- src/store/index.js
+++ src/store/index.js
@@ -12,6 +12,10 @@ export default createStore({
     },
     addToBag(state, product) {
       state.productsInBag.push(product)
+    },
+    removeFromBag(state, productId) {
+      var updatedBag = state.productsInBag.filter(item => productId != item.id)
+      state.productsInBag = updatedBag
     }
   },
   actions: {
@@ -26,6 +30,10 @@ export default createStore({
 
     addToBag({commit}, product){
       commit('addToBag', product)
+    },
+
+    removeFromBag({commit}, productId){
+      commit('removeFromBag', productId)
     }
 
   },
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -12,7 +12,11 @@
         <h4>{{product.title}}</h4>
         <p class="price">R$ {{product.price.toFixed(2)}}</p>
         <button v-if="!isInBag(product)" @click="addToBag(product)">Adicionar ao carrinho</button>
-        <button v-else class="remove">Remover do carrinho</button>
+        <button 
+        v-else 
+        class="remove"
+        @click="this.$store.dispatch('removeFromBag', product.id)"
+        >Remover do carrinho</button>
       </div>
     </div>
   </div>
