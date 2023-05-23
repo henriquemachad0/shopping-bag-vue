872b81058e4b7b5164bacb44ae7c58276172be0f
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-05-23 19:38:53 -0300

    feat: product lists in shopping bag (alter quantity and remove)

--- src/views/ShoppingBasket.vue
+++ src/views/ShoppingBasket.vue
@@ -2,26 +2,37 @@
   <div class="basket">
     <div class="items">
 
-      <div class="item">
-        <div class="remove">Remover Produto</div>
-        <div class="photo"><img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt=""></div>
-        <div class="description">Mens Casual Premium Slim Fit T-Shirts </div>
-        <div class="price">
-          <span class="quantity-area">
-            <button disabled="">-</button>
-            <span class="quantity">1</span>
-            <button>+</button>
-          </span>
-          <span class="amount">R$ 22.30</span>
+      <template v-if="productsInBag.length"> 
+          <div v-for="(product, index) in this.productsInBag" :key="index" class="item">
+          <div class="remove" @click="this.$store.dispatch('removeFromBag', product.id)">Remover Produto</div>
+          <div class="photo"><img :src="product.image" alt=""></div>
+          <div class="description">
+            {{product.title}} 
+          </div>
+          <div class="price">
+            <span class="quantity-area">
+              <button :disabled="product.quantity<=1" @click="product.quantity--">-</button>
+              <span class="quantity">{{product.quantity}}</span>
+              <button @click="product.quantity++">+</button>
+            </span>
+            <span class="amount">R$ {{ (product.price * product.quantity).toFixed(2)}}</span>
+          </div>
         </div>
-      </div>
-      <div class="grand-total"> Total do pedido: R$ 22.30</div>
+        <div class="grand-total"> Total do pedido: R$ 22.30</div>
+      </template>
 
+      <template v-else >
+        <h4>Não há items no carrinho ainda</h4>
+      </template>
+
+      
     </div>
   </div>
 </template>
 
 <script>
+
+import { mapState } from 'vuex'
 
 export default {
   name: 'ShoppingBasket',
@@ -29,6 +40,11 @@ export default {
   methods: {
    
   },
+
+  computed:  
+    mapState([
+      'productsInBag'
+    ]),
  
 }
 </script>
