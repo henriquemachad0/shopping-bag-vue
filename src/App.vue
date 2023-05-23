297e0c6832803a23f1df4711b76c998d967d456a
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-05-23 18:02:33 -0300

    feat: defining actions and mutation in vuex 1

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,10 @@
 
 <script>
 
-  import axios from 'axios'
-  
+
   export default {
     created(){
-      axios
-      .get('https://fakestoreapi.com/products')
-      .then(response =>{
-        console.log(response.data)
-      })
+      this.$store.dispatch('loadProducts')
     }
   }
   
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,24 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
-    
+    products: [1,2,3]
   },
   mutations: {
-    
+    loadProducts(state, products) {
+      state.products = products
+    }
   },
   actions: {
+
+    loadProducts({ commit}) {
+      axios
+      .get('https://fakestoreapi.com/products')
+      .then(response =>{
+        commit('loadProducts',response.data)
+      })
+    }
 
   },
   modules: {
